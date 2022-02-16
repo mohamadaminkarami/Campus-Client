2c1b51404caff5f00096427cc3e5415a88d70e6d
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2022-02-17 00:35:18 +0330

    fix naming

--- src/components/SchoolBoxField.jsx
+++ src/components/SchoolBoxField.jsx
@@ -1,16 +1,21 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import { useField } from "formik";
 import PropTypes from "prop-types";
 import { useRecoilValue } from "recoil";
-import schoolListState from "../states/schoolListState";
-import schoolFuseState from "../states/schoolFuseState";
+import Fuse from "fuse.js";
+import schoolListState from "../states/schoolsListState";
 import ErrorMessageField from "./ErrorMessageField";
 
 function SchoolBoxField(props) {
   const schoolsList = useRecoilValue(schoolListState);
-  const fuse = useRecoilValue(schoolFuseState);
+
+  const fuse = useMemo(() => {
+    console.log("SchoolBoxField fuse called");
+    return new Fuse(schoolsList, { keys: ["name"] });
+  }, [schoolsList]);
+
   const filterOptions = useCallback(
     (options, { inputValue }) => {
       return inputValue ? fuse.search(inputValue).map((i) => i.item) : options;
